f1f777ae2aaaa3be9266fa013748574a2cb0e71e
sifat007 <sifat27.ht@gmail.com>
2020-09-12 17:50:23 +0600

    collect resourse and design signin screen and splash screen

---
+++ app/src/main/java/com/example/mvvmbanglapart2/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mvvmbanglapart2.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mvvmbanglapart2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
