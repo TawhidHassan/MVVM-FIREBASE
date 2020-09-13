882b5af58432bbff39b7aac0d0ab58824c400df4
sifat007 <sifat27.ht@gmail.com>
2020-09-13 17:07:44 +0600

    user authenticate check and reduirect to signAcitvoity

--- app/src/main/java/com/example/mvvmbanglapart2/view/SplashActivity.java
+++ app/src/main/java/com/example/mvvmbanglapart2/view/SplashActivity.java
@@ -1,13 +1,55 @@
 package com.example.mvvmbanglapart2.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.mvvmbanglapart2.model.SignInUser;
+import com.example.mvvmbanglapart2.viewmodel.SignInViewModel;
+
 public class SplashActivity extends AppCompatActivity {
+
+    SignInViewModel signInViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initSpalashViewModel();
+        checkIfUserAuthenticate();
+    }
+
+    private void checkIfUserAuthenticate() {
+        signInViewModel.checkAutheticate();
+        signInViewModel.checkAuthenticateLivedata.observe(this, new Observer<SignInUser>() {
+            @Override
+            public void onChanged(SignInUser signInUser) {
+                if (!signInUser.isAuth)
+                {
+                    goToSignInActivity();
+                }else
+                {
+                    gotoMainActivity();
+                }
+            }
+        });
+    }
+
+    private void gotoMainActivity() {
+        Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void goToSignInActivity() {
+        Intent intent=new Intent(SplashActivity.this,SignInActivity.class);
+        startActivity(intent);
+        fileList();
+    }
+
+    private void initSpalashViewModel() {
+        signInViewModel=new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(SignInViewModel.class);
     }
 }
--- app/src/main/java/com/example/mvvmbanglapart2/viewmodel/SignInViewModel.java
+++ app/src/main/java/com/example/mvvmbanglapart2/viewmodel/SignInViewModel.java
@@ -4,9 +4,23 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mvvmbanglapart2.model.SignInUser;
+import com.example.mvvmbanglapart2.repository.SignInRepository;
 
 public class SignInViewModel extends AndroidViewModel {
+
+    private SignInRepository repository;
+    public LiveData<SignInUser> checkAuthenticateLivedata;
+
     public SignInViewModel(@NonNull Application application) {
         super(application);
+
+        repository=new SignInRepository();
+    }
+
+    public void checkAutheticate(){
+        checkAuthenticateLivedata=repository.checkAuthenticationInFirebase();
     }
 }
