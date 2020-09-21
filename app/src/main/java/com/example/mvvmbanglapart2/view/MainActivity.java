2ce711d4f669218be54714ee731f755e0396323a
sifat007 <sifat27.ht@gmail.com>
2020-09-21 18:29:45 +0600

    bottomNav with Navigation Component

--- app/src/main/java/com/example/mvvmbanglapart2/view/MainActivity.java
+++ app/src/main/java/com/example/mvvmbanglapart2/view/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.mvvmbanglapart2.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.mvvmbanglapart2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView= findViewById(R.id.bottomNavigationId);
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 }
