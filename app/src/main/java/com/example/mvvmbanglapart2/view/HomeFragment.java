51588de892f5dc76e6578d0c394f0aa931b0234a
sifat007 <sifat27.ht@gmail.com>
2020-09-23 17:29:08 +0600

    work on home fragment find section done

--- app/src/main/java/com/example/mvvmbanglapart2/view/HomeFragment.java
+++ app/src/main/java/com/example/mvvmbanglapart2/view/HomeFragment.java
@@ -2,15 +2,25 @@ package com.example.mvvmbanglapart2.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mvvmbanglapart2.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HomeFragment extends Fragment {
+    private Button signOutButton;
+    private CircleImageView profileImageView;
+    private TextView nameTextView,emailTextView;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,5 +32,25 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //find section
+        signOutButton= view.findViewById(R.id.signOutButtonId);
+        profileImageView= view.findViewById(R.id.profileImageId);
+        nameTextView= view.findViewById(R.id.profileNameId);
+        emailTextView= view.findViewById(R.id.profileEmailId);
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+            }
+        });
+
     }
 }
