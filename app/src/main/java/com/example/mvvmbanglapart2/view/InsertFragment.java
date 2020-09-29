492a577d1a7048245bb07ab11700d744f8403e7f
sifat007 <sifat27.ht@gmail.com>
2020-09-29 18:52:48 +0600

    insert data successfully done insert data function

--- app/src/main/java/com/example/mvvmbanglapart2/view/InsertFragment.java
+++ app/src/main/java/com/example/mvvmbanglapart2/view/InsertFragment.java
@@ -15,6 +15,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -36,22 +37,20 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import dmax.dialog.SpotsDialog;
 
 public class InsertFragment extends Fragment {
-
     private CircleImageView insertImageView;
     private EditText insertNameEditText;
     private EditText insertPhoneEditText;
     private EditText insertEmailEditText;
 
     private Button saveButton;
-    private static final int CAPTURE_PICCODE = 989;
     private Uri insertImageUri= null;
-
+    private static final int CAPTURE_PICCODE = 989;
+    //view Model
     private ContactViewModel contactViewModel;
 
     public InsertFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,7 +62,6 @@ public class InsertFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         initialViewModel();
 
         //find section...
@@ -72,54 +70,81 @@ public class InsertFragment extends Fragment {
         insertPhoneEditText= view.findViewById(R.id.insertPhoneId);
         insertEmailEditText= view.findViewById(R.id.insertEmailId);
         saveButton= view.findViewById(R.id.saveButtonId);
-
         insertImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 uploadImage();
             }
         });
-
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+
                 String id= randomDigit();
                 String name= insertNameEditText.getText().toString();
                 String phone= insertPhoneEditText.getText().toString();
                 String email= insertEmailEditText.getText().toString();
                 if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(phone) && !TextUtils.isEmpty(email) && insertImageUri != null){
-                    //show spots dialogue here.....
-                    final AlertDialog dialogue= new SpotsDialog.Builder().setContext(getActivity()).setTheme(R.style.Custom).setCancelable(true).build();
-                    dialogue.show();
 
-                    ContactUser user= new ContactUser(id,name,"image_uri",phone,email);
-                    contactViewModel.insert(user,insertImageUri);
-                    contactViewModel.insertResultLiveData.observe(getActivity(), new Observer<String>() {
-                        @Override
-                        public void onChanged(String s) {
-                            dialogue.dismiss();
-                            Toast.makeText(getActivity(), ""+s, Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                        //show spots dialogue here.....
+                        final AlertDialog dialogue= new SpotsDialog.Builder().setContext(getActivity()).setTheme(R.style.Custom).setCancelable(true).build();
+                        dialogue.show();
 
-                    insertImageView.setImageResource(R.drawable.profile);
-                    insertNameEditText.setText("");
-                    insertPhoneEditText.setText("");
-                    insertEmailEditText.setText("");
+                        ContactUser user= new ContactUser(id,name,"image_uri",phone,email);
+                        contactViewModel.insert(user,insertImageUri);
+                        contactViewModel.insertResultLiveData.observe(getActivity(), new Observer<String>() {
+                            @Override
+                            public void onChanged(String s) {
+                                dialogue.dismiss();
+                                Toast.makeText(getActivity(), ""+s, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        insertImageView.setImageResource(R.drawable.profile);
+                        insertNameEditText.setText("");
+                        insertPhoneEditText.setText("");
+                        insertEmailEditText.setText("");
 
 
                 }
                 else {
                     Toast.makeText(getActivity(), "Please Fill up all field", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
-
 
     }
 
     private void initialViewModel() {
+        contactViewModel= new ViewModelProvider(getActivity(),ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()))
+                .get(ContactViewModel.class);
+    }
 
+    private void uploadImage() {
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)!=
+                    PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+            }
+            else {
+                imagePick();
+            }
+        }
+        else {
+            imagePick();
+        }
+    }
+
+    private void imagePick() {
+
+        /*Intent intent= new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, CAPTURE_PICCODE);*/
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAspectRatio(1,1)
+                .start(getContext(),this);
     }
 
     //generate a random digit.........
@@ -138,29 +163,6 @@ public class InsertFragment extends Fragment {
     }
 
 
-    private void uploadImage() {
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
-        {
-            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
-            {
-                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
-            }else {
-                imagePick();
-            }
-        }
-    }
-
-    private void imagePick() {
-
-        /*Intent intent= new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, CAPTURE_PICCODE);*/
-        CropImage.activity()
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .setAspectRatio(1,1)
-                .start(getContext(),this);
-
-    }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -174,8 +176,7 @@ public class InsertFragment extends Fragment {
                 Exception error = result.getError();
             }
         }
-
-        /* if(resultCode== getActivity().RESULT_OK){
+       /* if(resultCode== getActivity().RESULT_OK){
             if(requestCode== CAPTURE_PICCODE){
                 Uri returnUri = data.getData();
                 Bitmap bitmapImage = null;
@@ -192,8 +193,5 @@ public class InsertFragment extends Fragment {
                     insertImageView.setImageBitmap(resize);
             }
         }*/
-
     }
-
-
 }
--- app/src/main/java/com/example/mvvmbanglapart2/viewmodel/ContactViewModel.java
+++ app/src/main/java/com/example/mvvmbanglapart2/viewmodel/ContactViewModel.java
@@ -1,4 +1,36 @@
 package com.example.mvvmbanglapart2.viewmodel;
 
-public class ContactViewModel {
+import android.app.Application;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mvvmbanglapart2.model.ContactUser;
+import com.example.mvvmbanglapart2.repository.ContactRepository;
+
+public class ContactViewModel extends AndroidViewModel {
+
+    private ContactRepository repository;
+
+    public LiveData<String >insertResultLiveData;
+
+
+    public ContactViewModel(@NonNull Application application) {
+        super(application);
+
+        repository=new ContactRepository();
+    }
+
+    public void insert(ContactUser user, Uri uri){
+        insertResultLiveData= repository.insertContacFireStore(user, uri);
+    }
+
+
+
+
+
+
+
 }
