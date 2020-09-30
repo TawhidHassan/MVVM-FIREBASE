bd394e5ce24bf5fe25c957a4aac84cae8cb9877c
sifat007 <sifat27.ht@gmail.com>
2020-09-30 17:43:07 +0600

    show data on list fragment

--- app/src/main/java/com/example/mvvmbanglapart2/view/ListFragment.java
+++ app/src/main/java/com/example/mvvmbanglapart2/view/ListFragment.java
@@ -1,5 +1,6 @@
 package com.example.mvvmbanglapart2.view;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,26 +8,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.mvvmbanglapart2.R;
+import com.example.mvvmbanglapart2.adapter.ContactAdapter;
 import com.example.mvvmbanglapart2.model.ContactUser;
 import com.example.mvvmbanglapart2.viewmodel.ContactViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import dmax.dialog.SpotsDialog;
+
 public class ListFragment extends Fragment {
 
     private SearchView searchView;
     private RecyclerView recyclerView;
     private List<ContactUser> userList= new ArrayList<>();
+    private ContactAdapter adapter;
     //view Model
     private ContactViewModel contactViewModel;
 
@@ -47,16 +53,27 @@ public class ListFragment extends Fragment {
         initViewModel();
         setupRecycle();
         //find Section...
-        searchView= view.findViewById(R.id.searchViewId);
+        searchView= (SearchView) view.findViewById(R.id.searchViewId);
         recyclerView= view.findViewById(R.id.recycleViewId);
+        adapter= new ContactAdapter();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
     private void setupRecycle() {
+        //show spots dialogue here.....
+        final AlertDialog dialogue= new SpotsDialog.Builder().setContext(getActivity()).setTheme(R.style.Custom).setCancelable(true).build();
+        dialogue.show();
         contactViewModel.show();
         contactViewModel.getContactLiveData.observe(getViewLifecycleOwner(), new Observer<List<ContactUser>>() {
             @Override
             public void onChanged(List<ContactUser> contactUsers) {
-                Toast.makeText(getActivity(), contactUsers.get(0).getContactName(), Toast.LENGTH_SHORT).show();
+                dialogue.dismiss();
+                userList= contactUsers;
+                //userList.clear();
+                adapter.getContactList(userList);
+                recyclerView.setAdapter(adapter);
+//                Toast.makeText(getActivity(), contactUsers.get(0).getContactName(), Toast.LENGTH_SHORT).show();
             }
         });
     }
